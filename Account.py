e3c84e97d991a6737ce9dda3c465357510aa93a2
ZARAXV <69867393+ZARAXV@users.noreply.github.com>
2023-04-20 20:07:27 -0500

    Added first file

---
+++ Account.py
@@ -0,0 +1,22 @@
+class Account:
+    def __init__(self, name: str) -> None:
+        """
+        Function to st up object
+        :param name: Account name
+        """
+
+        pass
+
+    def deposit(self, amount):
+        pass
+
+    def withdraw(self, amount):
+        pass
+
+    def get_balance(self):
+        pass
+
+    def get_name(self):
+        pass
+
+    

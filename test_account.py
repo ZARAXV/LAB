57fb2d48ad7f6bb4a46a4029eb358f35f92377ee
ZARAXV <69867393+ZARAXV@users.noreply.github.com>
2023-04-21 21:03:15 -0500

    Updated testing functions

--- test_account.py
+++ test_account.py
@@ -3,20 +3,35 @@ from Account import *
 
 class Test:
     def setup_method(self):
-        self.a1 = Account('John', 10)
-        self.a2 = Account('John', 20)
+        self.acc = Account('Jane')
 
     def teardown_method(self):
-        del self.a1
-        del self.a2
+        del self.acc
 
     def test_init(self):
-        pass
+        assert self.acc.get_name() == 'Jane'
+        assert self.acc.get_balance() == 0
 
     def test_deposit(self):
         #negative, zero, positive
-        pass
+        assert self.acc.deposit(-5) == False
+        assert self.acc.get_balance() == 0
+        assert self.acc.deposit(0) == False
+        assert self.acc.get_balance() == 0
+        assert self.acc.deposit(5) == True
+        assert self.acc.get_balance() == 5
 
     def test_withdraw(self):
-        #negative, zero, positive invalid, positve invalid
-        pass
+        #negative, zero, positive invalid
+        assert self.acc.deposit(10)
+        assert self.acc.withdraw(-5) == False
+        assert self.acc.get_balance() == 10
+        assert self.acc.withdraw(0) == False
+        assert self.acc.get_balance() == 10
+        assert self.acc.withdraw(50) == False
+        assert self.acc.get_balance() == 10
+        assert self.acc.withdraw(5) == True
+        assert self.acc.get_balance() == 5
+
+
+
